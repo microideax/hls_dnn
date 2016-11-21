38175611296531746fa48783a314592da5e123e8
Yao Chen <yaochen_nk@hotmail.com>
2016-11-21 13:02:31 +0800

    Added new run.sh script for easy testing.

--- examples/run.sh
+++ examples/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 cp -rf /home/yaochen/work/NN/NN_hls/tiny_dnn_LeNet5/eclipse_debug/src/tiny_dnn /home/yaochen/work/NN/NN_hls/tiny_dnn_LeNet5/
+
+g++ -std=c++11 -Wall -Wextra -pedantic -I ../ -O3 lenet.cpp -o hlstest
